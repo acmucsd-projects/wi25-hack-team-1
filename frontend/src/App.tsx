ae077e4d451842e0c547fd007ee54751d86a7747
yxli001 <lxd520anna@gmail.com>
2025-02-08 22:29:33 -0800

    add module alias and prettierrc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Layout from './components/Layout';
-import Login from './pages/Login';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home";
+import Layout from "@/components/Layout";
+import Login from "@/pages/Login";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home/>} />
-            <Route path="/login" element={<Login/>} />
-
-          </Route>        
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 };
 
-export default App
+export default App;
