e49933e2c2919a551ad391155da21b79b2a01a68
Sri Mente <srimente@gmail.com>
2025-03-01 18:22:23 -0800

    first commit

---
+++ backend/src/models/user.ts
@@ -0,0 +1,14 @@
+import { InferSchemaType, Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  _id: { type: String },
+  name: { type: String, required: true },
+  uni: { type: String },
+  email: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  gender: { type: String, required: true },
+});
+
+type User = InferSchemaType<typeof userSchema>;
+
+export default model<User>("User", userSchema);
