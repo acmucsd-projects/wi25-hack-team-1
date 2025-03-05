ce0e92048ecfce19efc95768e40d4db3c4a3ed8d
Ajay <ajay.ramkarthik@gmail.com>
2025-03-04 17:35:11 -0800

    Update auth_google_signin_popup.ts with email domain restriction and error handling

--- frontend/src/firebase/auth_google_signin_popup.ts
+++ frontend/src/firebase/auth_google_signin_popup.ts
@@ -8,7 +8,7 @@ const signInWithGooglePopup = () => {
     .then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
+      const token = credential ? credential.accessToken : null;
       // The signed-in user info.
       const user = result.user;
       console.log("User signed in:", user);
