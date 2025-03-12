3310df1a420a47e02c5474ffc09128207c4bfa58
Tanner Berman <berman.tanner@gmail.com>
2025-03-12 11:57:43 -0700

    refactored user and post models

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -2,8 +2,9 @@ import { InferSchemaType, Schema, model } from "mongoose";
 
 const userSchema = new Schema({
   uid: { type: String, required: true },
-  name: { type: String, required: true },
-  uni: { type: String },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  uni: { type: String, required: true},         // <TODO> Will need to be validated
   email: { type: String, required: true },
   phone: { type: String, required: true },
   gender: { type: String, required: true },
