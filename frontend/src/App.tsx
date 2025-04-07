c96a2d0f9050bf1c06682d5393a6872d71a1ddbf
Ajay <ajay.ramkarthik@gmail.com>
2025-04-07 14:31:24 -0700

    Changed app to include uber and added postcard layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,29 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Layout from "@/components/Layout";
 import Login from "@/pages/Login";
+import { Client as Styletron } from "styletron-engine-monolithic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider} from "baseui";
+
+
 
 const App: React.FC = () => {
+  const engine = new Styletron();
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </BaseProvider>
+    </StyletronProvider>
   );
 };
 
 export default App;
+
