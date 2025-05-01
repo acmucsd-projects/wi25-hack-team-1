d77c336cadce47dd14fe3fa3d6a61059d9eaddce
Tanner Berman <berman.tanner@gmail.com>
2025-04-30 17:40:30 -0700

    WIP Posts POST route

--- backend/src/routes/post.ts
+++ backend/src/routes/post.ts
@@ -7,3 +7,29 @@ import validationErrorParser from "@/utils/validationErrorParser";
 
 const router = express.Router();
 
+router.post(
+  "/",
+  verifyAuthToken,
+  async (req: RydeRequest, res: Response, next: NextFunction) => {
+    try {
+      const { flightDay, time, airport, luggage, numPassengers } = req.body;
+
+      const post = new Post({
+        creatorId: req.userId, // Authenticated user's ID
+        flightDay,
+        time,
+        airport,
+        luggage,
+        numPassengers,
+        passengers: [], // Start with an empty passengers array
+      });
+
+      const newPost = await post.save();
+      res.status(201).json(newPost);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+export { router as postRouter };
